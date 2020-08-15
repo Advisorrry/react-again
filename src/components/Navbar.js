835312f0ee024145494d398492bf2b80d75962c1
Advisorrry <sypikeh@yandex.com>
2020-08-15 19:30:17 +0500

    Господи за что ты придумал верстку

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navList'>
+      <ul>
+        <li><a href='#s'>Profile </a></li>
+        <li><a href='#s'>Messages </a></li>
+        <li><a href='#s'>News </a></li>
+        <li><a href='#s'>Music </a></li>
+        <li><a href='#s'>Settings </a></li>
+      </ul>
+    </div>
+    )
+}
+
+export default Navbar
