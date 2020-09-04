0279a6e17730b7f92fe81214af782d0a287b6834
Advisorrry <sypikeh@yandex.com>
2020-09-04 20:45:10 +0500

    action creator

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import m from './MyPosts.module.css'
-import {Post} from './Post/Post';
+import {Post} from './Post/Post'
+import {addPostActionCreator, updateNewPostTextActionCreator} from '../../../redux/state'
 
 export const MyPosts = (props) => {
 
@@ -10,20 +11,20 @@ export const MyPosts = (props) => {
     const newPostElement = React.createRef()
 
     const addPost = () => {
-        props.dispatch({type: 'ADD-POST'})
+        props.dispatch(addPostActionCreator())
 
     }
 
     const onPostChange = () => {
         const text = newPostElement.current.value
-        const action = {type: 'UPDATE-NEW-POST-TEXT', newText: text}
+        const action = updateNewPostTextActionCreator(text)
         props.dispatch(action)
     }
 
     return (
         <div className={m.posts}>
             <h5>My posts</h5>
-            <input ref={newPostElement} onChange={onPostChange} placeholder={props.newPostText} type="text" />
+            <input ref={newPostElement} onChange={onPostChange} value={props.newPostText} type="text"/>
             <button onClick={addPost} className="btn waves-effect waves-light" type="submit" name="action">send
                 <i className="material-icons right">send</i>
             </button>
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST'
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
+
 export const store = {
     _state: {
         profilePage: {
@@ -39,7 +42,7 @@ export const store = {
     },
 
     dispatch(action) {
-        if(action.type === 'ADD-POST') {
+        if (action.type === 'ADD-POST') {
             const newPost = {
                 id: 4,
                 text: this._state.profilePage.newPostText,
@@ -48,13 +51,19 @@ export const store = {
             this._state.profilePage.myPostData.push(newPost)
             // this._state.profilePage.newPostText = ''
             this._callSubscriber(this._state)
-        }else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
             this._state.profilePage.newPostText = action.newText
             this._callSubscriber(this._state)
         }
 
-
-
     }
 
 }
+
+export const addPostActionCreator = () => ({type: ADD_POST})
+
+export const updateNewPostTextActionCreator = (text) => ({
+        type: UPDATE_NEW_POST_TEXT, newText: text
+})
+
+
