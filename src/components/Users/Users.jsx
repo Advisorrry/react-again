69e5f2326f9a55bc23c9937a36abdc0a611c97f9
Advisorrry <sypikeh@gmail.com>
2020-10-16 15:20:23 +0500

    firebase, axios, class

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,86 +1,47 @@
 import React from 'react'
 import us from './Users.module.css'
+import * as axios from 'axios'
+import userPhoto from '../../assets/img/usersAvaAlexey.jpg'
 
-export const Users = (props) => {
+export class Users extends React.Component{
+    constructor(props) {
+        super(props)
 
-    if(props.users.length === 0) {
-        props.setusers([
-            {
-                id: 1,
-                usersAva: require('../../assets/img/usersAvaAlexey.jpg'),
-                followed: true,
-                firstName: 'Алексей',
-                lastName: 'Нэвэльный',
-                status: 'Новичок',
-                location: {county: 'Россия', city: 'Москва'}
-            },
-            {
-                id: 2,
-                usersAva: require('../../assets/img/usersAvaSonya.jpg'),
-                followed: true,
-                firstName: 'Соня',
-                lastName: 'Миронова',
-                status: 'Девочка-кокетка',
-                location: {county: 'Россия', city: 'Челябинск'}
-            },
-            {
-                id: 3,
-                usersAva: require('../../assets/img/usersAvaMaksim.jpg'),
-                followed: false,
-                firstName: 'Максим',
-                lastName: 'Горький',
-                status: 'АУФ',
-                location: {county: 'Россия', city: 'Санкт-Петербург'}
-            },
-            {
-                id: 4,
-                usersAva: require('../../assets/img/usersAvaAlexandr.jpg'),
-                followed: true,
-                firstName: 'Александр',
-                lastName: 'Лукашенко',
-                status: 'Старичок',
-                location: {county: 'Беларусь', city: 'Минск'}
-            },
-            {
-                id: 5,
-                usersAva: require('../../assets/img/usersAvaPutin.jpg'),
-                followed: true,
-                firstName: 'Владимир',
-                lastName: 'Путин',
-                status: 'Безумно можно быть первым',
-                location: {county: 'Россия', city: 'Кремль'}
-            },
-
-        ])
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setusers(response.data.items)
+        })
     }
 
 
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
-                <div className={us.block}>
-                    <div className={us.block__info}>
-                        <img className={us.usersPhoto} alt={`logo`} src={u.usersAva}/>
-                        <div className={us.info}>
-                            <p>{u.firstName} {u.lastName}<br/> {u.status}</p>
-                            <p>{u.location.county}, {u.location.city} </p>
 
+    render() {
+        return <div>
+            {
+                this.props.users.map(u => <div key={u.id}>
+                    <div className={us.block}>
+                        <div className={us.block__info}>
+                            <img className={us.usersPhoto} alt={`logo`} src={u.photos.small != null ? u.photos.small : userPhoto  }/>
+                            <div className={us.info}>
+                                <p>{u.name}<br/> {u.status}</p>
+                                <p>{'u.location.county'}, {'u.location.city'} </p>
+
+                            </div>
                         </div>
+
+                        <a href={'#f'} className={us.follow}>{u.followed
+                            ? <p onClick={() => {
+                                this.props.unfollow(u.id)
+                            }}>unfollow</p>
+                            : <p onClick={() => {
+                                this.props.follow(u.id)
+                            }}>follow</p>}
+                        </a>
                     </div>
 
-                    <a href={'#f'} className={us.follow}>{u.followed
-                        ? <p onClick={() => {
-                            props.unfollow(u.id)
-                        }}>unfollow</p>
-                        : <p onClick={() => {
-                            props.follow(u.id)
-                        }}>follow</p>}
-                    </a>
-                </div>
+                </div>)
+            }
+        </div>
 
-            </div>)
-        }
-    </div>
-
+    }
 
 }
